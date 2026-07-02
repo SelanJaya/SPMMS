3f989154ccc9b41de73987a4a1707e75a13d52f3
SelanJay <jayaselan0321@gmail.com>
2026-07-02 08:53:15 +0800

    cCommitemail service

--- WebApplication1/src/java/Service/TeamAssignmentService.java
+++ WebApplication1/src/java/Service/TeamAssignmentService.java
@@ -61,30 +61,30 @@ public class TeamAssignmentService {
                     + assign_by_username;
 
             // Send email asynchronously
-//            emailExecutor.submit(() -> {
-//                try {
-//                    EmailService emailService = new EmailService();
-//                    emailService.sendEmail(
-//                            projectTeamAssignment.getAsign_to_Email(),
-//                            subject,
-//                            message
-//                    );
-//                    System.out.println(
-//                            "Removal email sent to: "
-//                            + projectTeamAssignment.getAsign_to_Email()
-//                    );
-//
-//                } catch (Exception e) {
-//                    System.err.println(
-//                            "Failed to send removal email to: "
-//                            + projectTeamAssignment.getAsign_to_Email()
-//                    );
-//                    e.printStackTrace();
-//                }
-//            });
+            emailExecutor.submit(() -> {
+                try {
+                    EmailService emailService = new EmailService();
+                    emailService.sendEmail(
+                            projectTeamAssignment.getAsign_to_Email(),
+                            subject,
+                            message
+                    );
+                    System.out.println(
+                            "Removal email sent to: "
+                            + projectTeamAssignment.getAsign_to_Email()
+                    );
 
-            //EmailService emailService = new EmailService( projectTeamAssignment.getAsign_to_Email(), );
-            //emailService.sendEmail(taskAssignment., subject, messageText);
+                } catch (Exception e) {
+                    System.err.println(
+                            "Failed to send removal email to: "
+                            + projectTeamAssignment.getAsign_to_Email()
+                    );
+                    e.printStackTrace();
+                }
+            });
+
+//            EmailService emailService = new EmailService( projectTeamAssignment.getAsign_to_Email(), );
+//            emailService.sendEmail(taskAssignment., subject, messageText);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Exception occured in recruitementService :" + e);
@@ -120,29 +120,29 @@ public class TeamAssignmentService {
                     + assign_by_username;
 
             // Send email asynchronously
-//            emailExecutor.submit(() -> {
-//                try {
-//                    EmailService emailService = new EmailService();
-//
-//                    emailService.sendEmail(
-//                            projectTeamAssignment.getAsign_to_Email(),
-//                            subject,
-//                            message
-//                    );
-//
-//                    System.out.println(
-//                            "Removal email sent to: "
-//                            + projectTeamAssignment.getAsign_to_Email()
-//                    );
-//
-//                } catch (Exception e) {
-//                    System.err.println(
-//                            "Failed to send removal email to: "
-//                            + projectTeamAssignment.getAsign_to_Email()
-//                    );
-//                    e.printStackTrace();
-//                }
-//            });
+            emailExecutor.submit(() -> {
+                try {
+                    EmailService emailService = new EmailService();
+
+                    emailService.sendEmail(
+                            projectTeamAssignment.getAsign_to_Email(),
+                            subject,
+                            message
+                    );
+
+                    System.out.println(
+                            "Removal email sent to: "
+                            + projectTeamAssignment.getAsign_to_Email()
+                    );
+
+                } catch (Exception e) {
+                    System.err.println(
+                            "Failed to send removal email to: "
+                            + projectTeamAssignment.getAsign_to_Email()
+                    );
+                    e.printStackTrace();
+                }
+            });
 
         } catch (Exception e) {
             e.printStackTrace();
